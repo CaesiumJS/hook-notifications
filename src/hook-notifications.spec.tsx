d9fd0f1b2896930efe67fbc3daf473150704d730
Adam Laycock <adam@arcath.net>
2020-09-24 21:12:08 +0100

    initial commit

---
+++ src/hook-notifications.spec.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import {render, cleanup, fireEvent} from '@testing-library/react'
+
+import {useNotify, useNotifications, Notifications, useNotificationsFunction} from './hook-notifications'
+
+const TestWrapper: React.FC = ({children}) => {
+  return <Notifications>{children}</Notifications>
+}
+
+const TestComponent: React.FC = () => {
+  const {notifications, removeNotification} = useNotifications()
+  const {notify} = useNotify()
+
+  return <>
+    <p>Notifications: {notifications.length}</p>
+    {notifications[0] ? <p>UUID: {notifications[0].uuid}</p> : ''}
+    <button onClick={() => {
+      notify({
+        title: 'test',
+        message: 'Testing'
+      })
+    }}>add notification</button>
+    <button onClick={() => {
+      removeNotification(notifications[0].uuid)
+    }}>remove notification</button>
+  </>
+}
+
+describe('Notifications', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('should throw and exception if not a child of <otifications>', () => {
+    expect(() => {
+      render(<TestComponent />)
+    }).toThrow()
+  })
+
+  it('should add a notification with a uuid', () => {
+    const {getByText} = render(<TestWrapper><TestComponent /></TestWrapper>)
+
+    expect(getByText('Notifications: 0')).toBeInTheDocument()
+
+    fireEvent.click(getByText('add notification'))
+
+    expect(getByText('Notifications: 1')).toBeInTheDocument()
+    expect(getByText(/^UUID: [0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)).toBeInTheDocument()
+
+    fireEvent.click(getByText('remove notification'))
+
+    expect(getByText('Notifications: 0')).toBeInTheDocument()
+  })
+
+  it('should support custom notification types', () => {
+    const Component: React.FC = () => {
+      const {notifications} = (useNotifications as useNotificationsFunction<{
+        icon: string
+        title: string
+        message: string
+        uuid: string
+      }>)()
+
+      return <>
+        <p>Notifications: {notifications.length}</p>
+      </>
+    }
+
+    const {getByText} = render(<TestWrapper><Component /></TestWrapper>)
+
+    expect(getByText('Notifications: 0')).toBeInTheDocument()
+  })
+})
---
+++ src/hook-notifications.tsx
@@ -0,0 +1,90 @@
+import React, {createContext, useContext, useReducer} from 'react'
+import {v4 as uuidv4} from 'uuid'
+
+export interface BaseNotification{
+  title: string
+  message: string
+}
+
+export interface Notification extends BaseNotification{
+  uuid: string
+}
+
+interface ReducerAction<T, K>{
+  type: T
+  payload: K
+}
+
+type NotificationActions =
+  ReducerAction<'add', BaseNotification> |
+  ReducerAction<'remove', string>
+
+
+const initialState: Notification[] = []
+
+const NotificationsStateContext = createContext(initialState)
+const NotificationsDispatchContext = createContext<React.Dispatch<NotificationActions> | undefined>(undefined)
+
+const notificationReducer = (state: Notification[], action: NotificationActions) => {
+  let newState = ([] as Notification[]).concat(state)
+
+  switch(action.type){
+    case 'add':
+      let notification = Object.assign({uuid: uuidv4()}, action.payload)
+
+      newState.push(notification)
+      return newState
+    case 'remove':
+      return newState.filter((notification) => {
+        return notification.uuid !== action.payload
+      })
+  }
+}
+
+export const Notifications: React.FC = ({children}) => {
+  const [state, dispatch] = useReducer(notificationReducer, [])
+
+  return <NotificationsStateContext.Provider value={state}>
+    <NotificationsDispatchContext.Provider value={dispatch}>
+      {children}
+    </NotificationsDispatchContext.Provider>
+  </NotificationsStateContext.Provider>
+}
+
+export type useNotificationsFunction<T extends Notification = Notification> = () => {
+  notifications: T[]
+  removeNotification: (uuid: string) => void
+}
+
+export const useNotifications: useNotificationsFunction = () => {
+  const state = useContext(NotificationsStateContext)
+  const dispatch = useContext(NotificationsDispatchContext)
+
+  if(!dispatch){
+    throw new Error("useNotifications can only be called within <Notifications>")
+  }
+
+  const removeNotification = (uuid: string) => {
+    dispatch({type: 'remove', payload: uuid})
+  }
+
+  return {notifications: state, removeNotification}
+}
+
+export type UseNotifyFunction<T extends BaseNotification = BaseNotification> = () => {
+  notify: (notification: T) => void
+}
+
+export const useNotify: UseNotifyFunction = () => {
+  const dispatch = useContext(NotificationsDispatchContext)
+
+  if(!dispatch){
+    throw new Error("useNotifications can only be called within <Notifications>")
+  }
+
+  const notify = (notification: BaseNotification) => {
+    dispatch({type: 'add', payload: notification})
+  }
+
+  return {notify}
+}
